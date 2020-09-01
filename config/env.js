cceb99ef75ab6a6341c7367ec2f99e401be8e69e
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-08-31 21:09:04 -0700

    included cors, made for prod

--- config/env.js
+++ config/env.js
@@ -1,31 +1,31 @@
 //Uncomment for Dev
-const env = {
-  database: 'tech_site',
-  username: 'root',
-  password: 'Walruses8993',
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
- 
-//Uncomment for Production
 // const env = {
 //   database: 'tech_site',
-//   username: 'lightsage88',
-//   password: 'Walruses8993!',
-//   host: 'techsite.crsru2uefxmy.us-east-2.rds.amazonaws.com',
+//   username: 'root',
+//   password: 'Walruses8993',
+//   host: 'localhost',
 //   dialect: 'mysql',
 //   pool: {
-//     max: 100,
+//     max: 10,
 //     min: 0,
-//     acquire: 300000,
-//     idle: 100000
+//     acquire: 30000,
+//     idle: 10000
 //   }
 // };
+ 
+//Uncomment for Production
+const env = {
+  database: 'tech_site',
+  username: 'lightsage88',
+  password: 'Walruses8993!',
+  host: 'techsite.crsru2uefxmy.us-east-2.rds.amazonaws.com',
+  dialect: 'mysql',
+  pool: {
+    max: 100,
+    min: 0,
+    acquire: 300000,
+    idle: 100000
+  }
+};
 
 module.exports = env;
