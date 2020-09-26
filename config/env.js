e4003cfe06f0de1dfd8a6980d80daf749e0bde63
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-09-26 15:35:45 -0700

    aws db

--- config/env.js
+++ config/env.js
@@ -13,7 +13,7 @@
 //   }
 // };
  
-//Uncomment for Production
+//Uncomment for aws Production
 const env = {
   database: 'tech_site',
   username: 'lightsage88',
@@ -27,5 +27,20 @@ const env = {
     idle: 100000
   }
 };
+
+//Uncomment for SiteGround Production
+// const env = {
+//   database: 'adrianr1_techSite',
+//   username: 'adrianr1_develop',
+//   password: 'Walruses8',
+//   host: 'giow2.siteground.us',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 100,
+//     min: 0,
+//     acquire: 300000,
+//     idle: 100000
+//   }
+// };
 
 module.exports = env;
