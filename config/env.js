df2c184c8eca781989f00be6038d6476c9803222
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-09-26 14:26:09 -0700

    revert to old db

--- config/env.js
+++ config/env.js
@@ -15,10 +15,10 @@
  
 //Uncomment for Production
 const env = {
-  database: 'adrianr1_techSite',
-  username: 'adrianr1_develop',
-  password: 'Walruses8',
-  host: 'giow2.siteground.us',
+  database: 'tech_site',
+  username: 'lightsage88',
+  password: 'Walruses8993!',
+  host: 'techsite.crsru2uefxmy.us-east-2.rds.amazonaws.com',
   dialect: 'mysql',
   pool: {
     max: 100,
